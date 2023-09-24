7e2985eaa6d99f22f4613391a0f335aca421992c
Mreza <seyyedi98@outlook.com>
2023-09-24 12:46:06 +0330

    Finished building Home section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -22,6 +22,32 @@ function Home() {
           <span className="text-cs">I Am</span> <b>Web Developer</b>
         </p>
 
+        <div className="home__img-wrapper">
+          <div className="home__banner">
+            <img src={profileImg} alt="" className="home__profile" />
+          </div>
+
+          <p className="home__data home__data-one">
+            <span className="text-lg">
+              12 <b>+</b>
+            </span>
+            <span className="text-sm texs-cs">
+              Years of <span>Experience</span>
+            </span>
+          </p>
+
+          <p className="home__data home__data-two">
+            <span className="text-lg">330</span>
+            <span className="text-sm texs-cs">
+              Completed <span>Projects</span>
+            </span>
+          </p>
+
+          <img src={shapeOne} className="shape shape__1" />
+          <img src={shapeTwo} className="shape shape__2" />
+          <img src={shapeTwo} className="shape shape__3" />
+        </div>
+
         <p className="home__text">
           From Francec, Paris. I have rich experience in web design, also I am
           good at wordpress. I love to talk with you about our unique
@@ -40,10 +66,10 @@ function Home() {
         </div>
 
         <div className="home__btns">
-          <a href="" className="btn">
+          <a href="" className="btn text-cs">
             Download CV
           </a>
-          <a href="" className="hero__link">
+          <a href="" className="hero__link text-cs">
             My Skills
           </a>
         </div>
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,5 @@
+function Services() {
+  return <div>Ser</div>;
+}
+
+export default Services;
